9cac718ce495a6596cd918aee69978c757a8d126
ChengLiufeng <liufeng.cheng@ericsson.com>
2014-11-07 22:20:36 +0800

    add reset.less index.jade detail.jade

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'kissy-blog',
+    description: 'a blog use kissy and node'
+  });
 });
 
 module.exports = router;
